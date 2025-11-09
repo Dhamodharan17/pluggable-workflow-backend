754f8a08351da70f4722d2490a71c2a6fd22af55
dsundar17 <msdhamodharan5@gmail.com>
2025-11-09 23:40:28 +0530

    first push with simple hello controller

---
+++ src/main/java/com/pluggableworkflow/PluggableWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.pluggableworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluggableWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluggableWorkflowApplication.class, args);
+	}
+
+}
